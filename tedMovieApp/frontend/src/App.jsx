4702bb339a242d032a4ff820b1257ee169edce1b
Gyri Semb <gyri.semb@no.experis.com>
2026-02-10 13:31:59 +0100

    created app to show inputfields in browser

--- tedMovieApp/frontend/src/App.jsx
+++ tedMovieApp/frontend/src/App.jsx
@@ -1,35 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import InputField from './components/InputField'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [submitted, setSubmitted] = useState(false);
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+
+    setFormData(prev => ({
+      ...prev,
+      [name] : value,
+    }));
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitted(true);
+    console.log("Registration complete");
+    console.log("Submitted values: ", formData);
+  };
+
+  const inputFields = [
+    {name: "username", type: "text", label: "Username"},
+    {name: "password", type: "password", label: "Password"},
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className = "App">
+        {submitted ? (
+          <h2> Thank you for regiserting on our page {formData.username}.</h2>
+        ): (<form onSubmit = {handleSubmit}>
+        {inputFields.map((inputField) => (
+          <InputField
+              key = {inputField.name}
+              name = {inputField.name}
+              type = {inputField.type}
+              label = {inputField.label}
+              value = {formData[inputField.name]}
+              onChange = {handleChange}
+              />
+        ))}
+        <button type = "submit"> Register </button>
+        </form>
+        )}
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
   )
 }
 
 export default App
+
