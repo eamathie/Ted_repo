24e7ddbef2dd6938a75a0103de55f2d0375ca8b0
Emil Aron Andresen Mathiesen <emilmathie@yahoo.no>
2026-02-12 13:25:53 +0100

    removed unneeded log in OmdbApiService.cs

--- tedMovieApp/tedMovieApp/Services/OmdbApiService.cs
+++ tedMovieApp/tedMovieApp/Services/OmdbApiService.cs
@@ -5,8 +5,7 @@ using tedMovieApp.Tools;
 
 namespace tedMovieApp.Services;
 
-public class OmdbApiService(ILogger<OmdbApiService> logger, IOptions<OmdbSettings> settings)
-    : IOmdbApiService
+public class OmdbApiService(IOptions<OmdbSettings> settings) : IOmdbApiService
 {
     private readonly OmdbSettings _settings = settings.Value; 
     private readonly HttpClient _httpClient = new();
@@ -14,16 +13,12 @@ public class OmdbApiService(ILogger<OmdbApiService> logger, IOptions<OmdbSetting
     public async Task<string> GetMoviesByQuery(string query)
     {
         var url = $"{_settings.BaseUrl}?apiKey={_settings.ApiKey}&s={query}"; 
-        logger.LogInformation("Calling OMDB API: {url}", url); 
-        
         return await _httpClient.GetStringAsync(url);
     }
 
     public async Task<string> GetMovieById(string id)
     {
         var url = $"{_settings.BaseUrl}?apiKey={_settings.ApiKey}&i={id}"; 
-        logger.LogInformation("Calling OMDB API: {url}", url); 
-        
         return await _httpClient.GetStringAsync(url);
     }
 }
