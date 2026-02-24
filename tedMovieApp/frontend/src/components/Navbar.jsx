b9d32f3ef888b0336d6e5afff5884aa93756a3e6
Gyri Semb <gyri.semb@no.experis.com>
2026-02-24 13:58:15 +0100

    fixed logout button

--- tedMovieApp/frontend/src/components/Navbar.jsx
+++ tedMovieApp/frontend/src/components/Navbar.jsx
@@ -1,15 +1,28 @@
-import { Routes, Route, Link } from "react-router-dom";
-import './Navbar.css';
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { useAuth } from "../auth/AuthContext";
 
 export default function Navbar() {
+  const { user, logout } = useAuth();
 
   return (
     <div className="topnav">
-      <a className="active" href="#home">Home</a>
-      <a href="#profile">Profile</a>
+      <div className="topnav-left">
+        <Link className="nav-item" to="/">Home</Link>
+        <Link className="nav-item" to="/profile">Profile</Link>
+        <Link className="nav-item" to="/my_reviews"> My reviews</Link>
+
+        {/* ONLY show logout when user exists */}
+        {user && (
+          <button className="nav-item" onClick={logout}>
+            Log out
+          </button>
+        )}
+      </div>
+
+      <div className="topnav-right" />
     </div>
   );
 }
-
 
 
